ea13c0508618eba48fdc16dfccb2b60879e5e978
24117 <77130049+24117@users.noreply.github.com>
2021-03-12 01:47:54 -0800

    revert

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const PASSCODE = "1234";
 
+let groceryItemArray = [];
+
+// define a constructor to create groceryItem objects
+let GroceryItemObject = function (pName, pQuantity, pCategory, pNote) {
+    this.ID = Math.random().toString(16).slice(5)  // tiny chance could get duplicates!
+    this.Name = pName;
+    this.Quantity = pQuantity;
+    this.Category = pCategory;  // household  produce  frozenFood  Beverage dairy  bakery  meat DryCannedFood snack
+    this.Note = pNote;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
 
 // login
 let userInputedPasscode = "";
-while(userInputedPasscode.trim() != PASSCODE || userInputePasscode == "") {
+while(userInputedPasscode.trim() != PASSCODE) {
     userInputedPasscode = prompt("Please enter your passcode");
 } 
 
