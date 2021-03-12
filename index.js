bc923a33dc286b4ce5fe7d61433eb4a2beec8657
katiepham4 <katiepham4@gmail.com>
2021-03-11 23:56:28 -0800

    most updated version

--- index.js
+++ index.js
@@ -37,9 +37,10 @@ let groceryArray = [];
 let GroceryItem = function (pName, pQuantity, pCategory, pNote) {
     this.Name = pName;
     this.Quantity = pQuantity;
-    this.Category = pCategory;  // action  comedy  drama  horrow scifi  musical  western
+    this.Category = pCategory;  
     this.Note = pNote;
 }
+
 
 let selectedQuantity = "1";
 let selectedCategory = "household";
@@ -54,23 +55,25 @@ document.addEventListener("DOMContentLoaded", function () {
         groceryArray.push(new GroceryItem(document.getElementById("name").value, 
             selectedQuantity, selectedCategory, document.getElementById("note").value));
         document.location.href = "index.html#ListAll";
-        // also add the URL value
     });
     
     document.getElementById("buttonClear").addEventListener("click", function () {
         document.getElementById("name").value = "";
-    });
-
-    $(document).bind("change", "#category", function (event, ui) {
-        selectedCategory = $('#category').val();
+        document.getElementById("quantity").value = "";
+        document.getElementById("category").value = "";
+        document.getElementById("note").value = "";
     });
 
     $(document).bind("change", "#quantity", function (event, ui) {
-        selectedQuantity= $('#quantity').val();
+        selectedGenre = $('#quantity').val();
+    });
+
+    $(document).bind("change", "#category", function (event, ui) {
+        selectedGenre = $('#category').val();
     });
 
     document.getElementById("delete").addEventListener("click", function () {
-        deleteItem(document.getElementById("IDparmHere").innerHTML);
+        deleteGrocery(document.getElementById("IDparmHere").innerHTML);
         createList();  // recreate li list after removing one
         document.location.href = "index.html#ListAll";  // go back to movie list 
     });
@@ -98,16 +101,21 @@ document.addEventListener("DOMContentLoaded", function () {
         createList();
     });
 
-    
+    $(document).on("pagebeforeshow", "#ViewDetail", function (event) {   // have to use jQuery 
+        // clear prior data
+        var divGroceryList = document.getElementById("divGroceryListSubset");
+        while (divGroceryList.firstChild) {    // remove any old data so don't get duplicates
+            divGroceryList.removeChild(divGroceryList.firstChild);
+        };
+    });
 
     // need one for our details page to fill in the info based on the passed in ID
     $(document).on("pagebeforeshow", "#ViewDetail", function (event) {   // have to use jQuery 
-        let localID = document.getElementById("IDparmHere").innerHTML;
-        let arrayPointer = GetArrayPointer(localID);
-        document.getElementById("name").innerHTML =  groceryArray[arrayPointer].Name;
-        document.getElementById("quantity").innerHTML = groceryArray[arrayPointer].Quantity;
-        document.getElementById("category").innerHTML =  groceryArray[arrayPointer].Category;
-        document.getElementById("note").innerHTML =  + groceryArray[arrayPointer].Note;
+        //let arrayPointer = GetArrayPointer(localID);
+        document.getElementById("inputtedName").innerHTML = "The title is: " +groceryArray[arrayPointer].Name;
+        document.getElementById("inputtedQuantity").innerHTML = "Year released: " + groceryArray[arrayPointer].Quantity;
+        document.getElementById("inputtedCategory").innerHTML = "Genre: " + groceryArray[arrayPointer].Category;
+        document.getElementById("inputtedNote").innerHTML = "Leading Woman: " + groceryArray[arrayPointer].Note;
     });
  
 // end of page before show code *************************************************************************
@@ -115,9 +123,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });  
 // end of wait until document has loaded event  *************************************************************************
 
-// next 2 functions could be combined into 1 with a little work
-// such as I could pass in a variable which said which divMovieList div it should draw
-// to, and if no value is passed in to subset too, I could just include all.
 
 function createList() {
     // clear prior data
@@ -131,11 +136,8 @@ function createList() {
     groceryArray.forEach(function (element,) {   // use handy array forEach method
         var li = document.createElement('li');
         // adding a class name to each one as a way of creating a collection
-        li.classList.add('oneItem'); 
-        // use the html5 "data-parm" to encode the ID of this particular data object
-        // that we are building an li from
-        li.setAttribute("data-parm", element.Name);
-        li.innerHTML = element.Name + ":  " + element.Category + "  " + element.Quantity + "" +element.Note;
+        li.classList.add('inputtedName'); 
+        li.innerHTML = element.Name + ":" + element.Category ;
         ul.appendChild(li);
     });
     divGroceryList.appendChild(ul)
@@ -143,19 +145,88 @@ function createList() {
     // now we have the HTML done to display out list, 
     // next we make them active buttons
     // set up an event for each new li item, 
-    var liArray = document.getElementsByClassName("oneItem");
+    var liArray = document.getElementsByClassName("inputtedName");
     Array.from(liArray).forEach(function (element) {
         element.addEventListener('click', function () {
-        // get that data-parm we added for THIS particular li as we loop thru them
-        var parm = this.getAttribute("data-parm");  // passing in the record.Id
-        // get our hidden <p> and write THIS ID value there
-        document.getElementById("IDparmHere").innerHTML = parm;
-        // now jump to our page that will use that one item
         document.location.href = "index.html#ViewDetail";
         });
     });
 
 };
+
+function deleteItem(which) {
+    console.log(which);
+    let arrayPointer = GetArrayPointer(which);
+    groceryArray.splice(arrayPointer, 1);  // remove 1 element at index 
+}
+
+// cycles thru the array to find the array element with a matching ID
+function GetArrayPointer(localID) {
+    for (let i = 0; i < groceryArray.length; i++) {
+        if (localID === groceryArray[i].ID) {
+            return i;
+        }
+    }
+}
+  
+
+function createListSubset(whichType) {
+    // clear prior data
+    var divGroceryList = document.getElementById("divGroceryListSubset");
+    while (divGroceryList.firstChild) {    // remove any old data so don't get duplicates
+        divGroceryList.removeChild(divGroceryList.firstChild);
+    };
+
+    var ul = document.createElement('ul');
+
+    groeryArray.forEach(function (element,) {
+        
+        if (element.Category === whichType) {
+            // use handy array forEach method
+            var li = document.createElement('li');
+            // adding a class name to each one as a way of creating a collection
+            li.classList.add('inputtedName');
+            li.innerHTML = element.Name + "  " + element.Category;
+            ul.appendChild(li);
+        }
+    });
+    divMovieList.appendChild(ul)
+
+    // now we have the HTML done to display out list, 
+    // next we make them active buttons
+    // set up an event for each new li item, 
+    var liArray = document.getElementsByClassName("inputtedName");
+    Array.from(liArray).forEach(function (element) {
+        element.addEventListener('click', function () {
+            document.location.href = "index.html#ViewDetail";
+        });
+    });
+
+};
+
+/**
+ *  https://ourcodeworld.com/articles/read/764/how-to-sort-alphabetically-an-array-of-objects-by-key-in-javascript
+* Function to sort alphabetically an array of objects by some specific key.
+* 
+* @param {String} property Key of the object to sort.
+*/
+function dynamicSort(property) {
+    var sortOrder = 1;
+
+    if (property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+
+    return function (a, b) {
+        if (sortOrder == -1) {
+            return b[property].localeCompare(a[property]);
+        } else {
+            return a[property].localeCompare(b[property]);
+        }
+    }
+}
+
 
     
     
